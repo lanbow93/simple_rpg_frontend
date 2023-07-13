a79bfc418fc85961f6a2f430fe095379abc969d0
lanbow93 <the.doctor1501@gmail.com>
2023-07-13 14:12:00 -0500

    all photos added to repo

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom"
 
-import { warriorImage, rogueImage, wizardImage } from "../utils/url"
+import warriorIcon from "../assets/warrior.png"
+import wizardIcon from "../assets/wizard.png"
+import rogueIcon from "../assets/rogue.png"
+
 
 
 function CharacterCard(props) {
     const character = props.data
     return <div className="characterCardArea">
         <h1>{character.name}</h1>
-        <img src={character.classType === "wizard" ? wizardImage : character.classType === "warrior" ? warriorImage : rogueImage} alt="" />
+        <img src={character.classType === "wizard" ? wizardIcon : character.classType === "warrior" ? warriorIcon : rogueIcon} alt="" />
         <div><Link to="/play" state={{user:character}} ><button>Play</button></Link></div>
         <div className="cardDetails">
             <p>Class:</p>
--- src/utils/url.jsx
+++ src/utils/url.jsx
@@ -1,8 +1,2 @@
 export const URL = "https://rpg-backend-933m.onrender.com"
 //  const url = "http://localhost:7755"
-
-export const warriorImage = "https://www.transparentpng.com/thumb/the-ultimate-warrior/X1Esmm-the-ultimate-warrior-clipart-png-photos.png"
-export const wizardImage = "https://www.pngall.com/wp-content/uploads/2016/06/Wizard-Free-Download-PNG.png"
-export const rogueImage = "https://www.nicepng.com/png/full/266-2664043_m-rogue-assassin-crossbow-shortsword-rogue-short-sword.png"
-
-
