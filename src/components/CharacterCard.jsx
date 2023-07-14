bc63cf50a1460f973bdfcb2d536d758dac2a2122
lanbow93 <the.doctor1501@gmail.com>
2023-07-13 21:56:43 -0500

    confim delete pop up, appears and closes

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-
+import { useState } from "react"
 import warriorIcon from "../assets/warrior.png"
 import wizardIcon from "../assets/wizard.png"
 import rogueIcon from "../assets/rogue.png"
@@ -8,8 +8,21 @@ import rogueIcon from "../assets/rogue.png"
 
 function CharacterCard(props) {
     const character = props.data
+
+    const [modalClass, setModalClass] = useState("hidden confirmDeletion")
+    
+    // When cancel button is pressed on confirm deletion screen
+    function handleClick(event) {
+        event.preventDefault()
+        setModalClass("hidden confirmDeletion")
+    }
+
+
     return <div className="characterCardArea">
-        <h1>{character.name}</h1>
+        <div className="banner">
+            <h1>{character.name}</h1>
+            <button onClick={(e) => setModalClass("confirmDeletion")}>X</button>
+        </div>
         <img src={character.classType === "wizard" ? wizardIcon : character.classType === "warrior" ? warriorIcon : rogueIcon} alt="" />
         <div><Link to="/play" state={{user:character}} ><button>Play</button></Link></div>
         <div className="cardDetails">
@@ -17,6 +30,13 @@ function CharacterCard(props) {
             <p>Level:</p>
             <p className="details">{character.classType.charAt(0).toUpperCase() + character.classType.substring(1)}</p>
             <p className="details">{Math.floor(character.experience / 10) + 1}</p>
+        </div>
+        <div className={modalClass}>
+        <form action={`/delete/${character._id}`} method="post">
+            <h2>Confirm Deletion</h2>
+            <button className="delete">Delete</button>
+            <button onClick={(e) => { handleClick(e)  }}>Cancel</button>
+        </form>
         </div>
     </div>
 }
