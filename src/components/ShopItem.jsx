e39e72fa2518ebb8cf98fbf7fe16be041b9e758a
lanbow93 <the.doctor1501@gmail.com>
2023-07-18 13:24:34 -0500

    app working before mapping store items

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,4 +1,8 @@
 import metalPlatingIcon from "../assets/metalPlatingIcon.png"
+import maceIcon from "../assets/maceIcon.png"
+import weakHealthPotionIcon from "../assets/weakHealthPotionIcon.png"
+
+
 function ShopItem(props){
     return <div className="shopItem">
         <img src={metalPlatingIcon} alt="" />
