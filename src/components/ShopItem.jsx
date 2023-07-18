2764ff4d3135eabc034c4f01dc93c31cf32494aa
lanbow93 <the.doctor1501@gmail.com>
2023-07-18 10:01:12 -0500

    store page style tweaked

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -2,7 +2,7 @@ import armorIcon from "../assets/armorIcon.png"
 function ShopItem(props){
     return <div className="shopItem">
         <img src={armorIcon} alt="" />
-        <p>10</p>
+        <p>10 Gold</p>
     </div>
 }
 
