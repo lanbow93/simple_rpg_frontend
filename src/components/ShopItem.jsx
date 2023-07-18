646a6e8501d28d031cad115e3e3da5a746a92e6d
lanbow93 <the.doctor1501@gmail.com>
2023-07-18 17:33:07 -0500

    switch statement added for store icons

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,12 +1,69 @@
 import metalPlatingIcon from "../assets/metalPlatingIcon.png"
 import maceIcon from "../assets/maceIcon.png"
 import weakHealthPotionIcon from "../assets/weakHealthPotionIcon.png"
+import healthPotionIcon from "../assets/healthPotionIcon.png"
+import strongHealthPotionIcon from "../assets/strongHealthPotionIcon.png"
 
+
+function determineImageToUse(itemName){
+    switch (itemName) {
+        // Weapons
+        case "staff":
+            return maceIcon          
+        case "grimoire":
+            return maceIcon
+        case "relic":
+            return maceIcon
+        case "mace":
+            return maceIcon
+        case "broadsword":
+            return maceIcon
+        case "master sword":
+            return maceIcon
+        case "poisoned bow":
+            return maceIcon
+        case "dagger":
+            return maceIcon
+        case "poisoned dagger":
+            return maceIcon
+        // Armor
+        case "novice robe":
+            return metalPlatingIcon
+        case "apprentice robe":
+            return metalPlatingIcon
+        case "journeyman robe":
+            return metalPlatingIcon
+        case "archmage robe":
+            return metalPlatingIcon
+        case "metal plating":
+            return metalPlatingIcon
+        case "diamond plating":
+            return metalPlatingIcon
+        case "adamantine plating":
+            return metalPlatingIcon
+        case "assassin's mantle":
+            return metalPlatingIcon
+        case "veil of shadows":
+            return metalPlatingIcon
+        case "reaper's robe":
+            return metalPlatingIcon
+        // Potions
+        case "weak potion":
+            return weakHealthPotionIcon
+        case "potion":
+            return healthPotionIcon
+        case "strong potion":
+            return strongHealthPotionIcon
+        default:
+            break;
+    }
+
+}
 
 function ShopItem(props){
-    console.log(props)
+    
     return <div className="shopItem">
-        <img src={props.itemType === "weapons" ? maceIcon : props.itemType === "armors" ? metalPlatingIcon : weakHealthPotionIcon} alt={`image of ${props.item}`} />
+        <img src={ determineImageToUse(props.item)} />
         <p>{props.cost} Gold</p>
     </div>
 }
