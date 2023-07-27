e4eb7b63d10ca0a36629f6ac6655ad8dba6278f3
lanbow93 <the.doctor1501@gmail.com>
2023-07-27 14:08:26 -0500

    created fightDisplay component

---
+++ src/components/FightScreen.jsx
@@ -0,0 +1,12 @@
+function FightScreen(props){
+    return <div className="fightDisplay">
+        <div className="enemyArea">
+            <h1>Enemy Area</h1>
+        </div>
+        <div className="userArea">
+            <h1>Friend Area</h1>
+        </div>
+    </div>
+}
+
+export default FightScreen
