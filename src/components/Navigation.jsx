b59c4ce1d77c530897df9d5157964aeefb4bee05
lanbow93 <the.doctor1501@gmail.com>
2023-08-11 13:58:55 -0500

    navigation closes after selecting menu option

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,15 +7,17 @@ const hiddenOptions = <section className="options hidden">
     <Link><h2>Logout</h2></Link>
 </section>
 
-const shownOptions = <section className="options">
-    <Link to="/" ><h2>Login</h2></Link>
-    <Link to="/characters" ><h2>Characters</h2></Link>
-    <Form action="/logout" method="post"><button><h2>Logout</h2></button></Form>
-</section>
+
 
 function Navigation(props){
     const [menuStatus, setMenuStatus] = useState("hidden")
     const menuClicked = () => menuStatus === "hidden" ? setMenuStatus("showing") : setMenuStatus("hidden")
+
+    const shownOptions = <section className="options">
+    <Link to="/" onClick={menuClicked} ><h2>Login</h2></Link>
+    <Link to="/characters" onClick={menuClicked}><h2>Characters</h2></Link>
+    <Form action="/logout" method="post" ><button ><h2>Logout</h2></button></Form>
+    </section>
 
     return <div className="navigation">
         <div className="dropdownMenu mobileOnly">
